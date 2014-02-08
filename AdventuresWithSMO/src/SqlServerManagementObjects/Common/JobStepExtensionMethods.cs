6b83ddfb36e2ffff56a3b4a0b74612b64be25681
Adam Grande <adam.grande@outlook.com>
2014-02-08 14:43:59 +0800

    Added SQL Server Management Objects source
    Basic code for interacting with SQL Server Agent related objects.

---
+++ AdventuresWithSMO/src/SqlServerManagementObjects/Common/JobStepExtensionMethods.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SqlServer.Management.Smo.Agent;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlservermanagementobjects.Common
+{
+    public static class JobStepExtensionMethods
+    {
+        public static JobStep GetLastFailedStep(this Job failedJob)
+        {
+            if (failedJob == null)
+                throw new ArgumentNullException("failedJob cannot be null");
+
+            if (failedJob.JobSteps == null || failedJob.JobSteps.Count == 0)
+                throw new ArgumentNullException("failedJob steps cannot be null or 0");
+
+
+            for (int i = (failedJob.JobSteps.Count-1); i >= 0; --i)
+            {
+                if (failedJob.JobSteps[i].LastRunOutcome == CompletionResult.Failed)
+                    return failedJob.JobSteps[i];
+            }
+            
+            return null;
+        }
+
+        public static string GetLastFailedMessageFromJob(this Job failedJob)
+        {
+            if (failedJob == null)
+                throw new ArgumentNullException("failedJob cannot be null");
+
+            JobStep failedStep = failedJob.GetLastFailedStep();
+
+            if (failedStep == null)
+                throw new ArgumentNullException("No failed step found for job " + failedJob.Name);
+            
+            DataTable data = failedStep.EnumLogs();
+            JobHistoryFilter filter = new JobHistoryFilter()
+            {
+                JobID = failedJob.JobID,
+                OldestFirst = false,
+                OutcomeTypes = CompletionResult.Failed
+
+            };
+
+            var jobHistory = failedJob.EnumHistory(filter).Select("StepName='" + failedStep.Name + "'", "");
+
+            if (jobHistory != null)
+                return jobHistory[0]["Message"].ToString();
+
+            return string.Empty;
+        }
+    }
+}
---
+++ AdventuresWithSMO/src/SqlServerManagementObjects/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Smo;
+using System.Threading;
+using Microsoft.SqlServer.Management.Smo.Agent;
+using sqlservermanagementobjects.Common;
+using System.Data;
+namespace sqlservermanagementobjects
+{
+    class Program
+    {
+        const string _server = "(local)";       // this will only work if you are running a default local sql instance, otherwise substitute your given server here.
+        const string _job = "MyAwesomeJob";
+
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                WriteLine("Connecting to " + _server + " instance of SQL Server 2008...", ConsoleColor.Green);
+
+                Server server = new Server(_server);
+
+                if (server.ConnectionContext.IsOpen)
+                {
+                    WriteLine("Oh no, something weird happened! The Server object shouldnt be opened at this point!", ConsoleColor.Red);
+                    return;
+                }
+                WriteLine("Server object is active (but not connected)!",  ConsoleColor.Green);
+
+                WriteLine("Retrieving '" + _job + "' SQL Job on the local SQL Agent.", ConsoleColor.Green);
+                var job = server.JobServer.Jobs[_job];
+
+                WriteLine("Job object is active.", ConsoleColor.Green);
+                job.Start();
+
+                Thread.Sleep(1000); // I realise this isnt ideal, but there can be a delay between the 
+                                    //.Start() and the status of the Job updating correctly, indicating the job is being executed.
+
+                job.Refresh();
+
+                while (true)
+                {
+
+                    if (job.CurrentRunStatus != JobExecutionStatus.Executing)
+                    {
+                        if (job.LastRunOutcome == CompletionResult.Succeeded)
+                            WriteLine("Hooray! Job Successful", ConsoleColor.Cyan);
+                        else
+                        {
+                            WriteLine("Oh Noes! Job failed!", ConsoleColor.Red);
+                            WriteLine("Job failed with message: " + job.GetLastFailedMessageFromJob(), ConsoleColor.Red);
+                        }
+                           
+                        break;
+                    }
+                    job.Refresh();      // You must call refresh each iteration, as the property doesnt update automatically.
+
+                    WriteLine("Waiting...", ConsoleColor.Yellow);
+                    Thread.Sleep(1000);
+                }
+            }
+            catch(Exception ex)
+            {
+                WriteLine("An error occurred.", ConsoleColor.Red);
+                WriteLine("Message: " + ex.Message, ConsoleColor.Red);
+            }
+
+            WriteLine("Press any key to exit.", ConsoleColor.Cyan);
+            Console.ReadKey();
+        }
+
+        static void WriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+        }
+
+    }
+}
